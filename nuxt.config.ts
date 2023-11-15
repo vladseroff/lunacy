cc5999e2082b0179c7970376e6344ec17c28d417
vladislav serov <vladseroff@gmail.com>
2023-11-15 21:25:11 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
                 { name: 'format-detection', content: 'telephone=no' }
             ],
             link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+                { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
             ]
         },
         baseURL: '/lunacy/',
